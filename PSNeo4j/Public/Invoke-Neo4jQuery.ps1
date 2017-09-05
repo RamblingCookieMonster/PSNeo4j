cef3fb55c69f2dfbff2deb8fead54af31b9ae28e
RamblingCookieMonster <ramblingcookiemonster@users.noreply.github.com>
2017-09-05 10:21:37 -0400

    Add initial comment based help to Invoke-Neo4jQuery

--- PSNeo4j/Public/Invoke-Neo4jQuery.ps1
+++ PSNeo4j/Public/Invoke-Neo4jQuery.ps1
@@ -1,4 +1,71 @@
 ﻿function Invoke-Neo4jQuery {
+    <#
+    .SYNOPSIS
+       Invoke Neo4j Cypher queries
+    .DESCRIPTION
+       Invoke Neo4j Cypher queries
+
+       Simplifies queries against the transactional Cypher HTTP endpoint.
+       Begins and commits a transaction in one request via $BaseUri/db/data/transaction/commit
+    .EXAMPLE
+        Invoke-Neo4jQuery -Query "MATCH (n) RETURN n"
+
+        # A simple query
+    .EXAMPLE
+        Invoke-Neo4jQuery -Query "MATCH (n:Server) WHERE n.ComputerName = `$ComputerName RETURN n" -Parameters @{ComputerName = 'dc01'}
+       
+        # A simple query with parameters
+    .PARAMETER Statements
+        One or more statements (hashtable or objects work) to invoke in a single 'Statements' call
+
+        Generally, the Query/Parameters parameters are simpler to use
+
+        Details: http://neo4j.com/docs/developer-manual/current/http-api/#rest-api-execute-multiple-statements
+
+    .PARAMETER Query
+        One or more Cypher queries to invoke
+
+        Be sure to escape any $ signs used for parameters
+
+    .PARAMETER Parameters
+        Parameters to pass for the specified Cypher query
+
+        Example:
+            -Query "... blah.Something = `$Whatever..."
+            -Parameters @{
+                Whatever = 'This replaces $Whatever'
+            }
+
+    .PARAMETER As
+        Parse the Neo4j response as...
+            Parsed:  We attempt to parse the output into friendlier PowerShell objects
+                     Please open an issue if you see unexpected output with this
+            Raw:     We don't touch the response                           ($Response)
+            Results: We expand the 'results' property on the response      ($Response.results)
+            Row:     We expand the 'row' property on the responses results ($Response.results.data.row)
+
+        We default to the value specified by Set-PSNeo4jConfiguration (Initially, 'Parsed')
+
+        See ConvertFrom-Neo4jResponse for implementation details
+    .PARAMETER MetaProperties
+        Merge zero or any combination of these corresponding meta properties in the results: 'id', 'type', 'deleted'
+
+        We default to the value specified by Set-PSNeo4jConfiguration (Initially, 'type')
+    .PARAMETER MergePrefix
+        If any MetaProperties are specified, we add this prefix to avoid clobbering existing neo4j properties
+
+        We default to the value specified by Set-PSNeo4jConfiguration (Initially, 'Neo4j')
+    .PARAMETER BaseUri
+        BaseUri to build REST endpoint Uris from
+
+        We default to the value specified by Set-PSNeo4jConfiguration (Initially, 'http://127.0.0.1:7474')
+    .PARAMETER Credential
+        PSCredential to use for auth
+
+        We default to the value specified by Set-PSNeo4jConfiguration (Initially, not specified)
+    .FUNCTIONALITY
+        Neo4j
+    #>
     [cmdletbinding(DefaultParameterSetName = 'Query')]
     param(
         [parameter(ParameterSetName='Statements',

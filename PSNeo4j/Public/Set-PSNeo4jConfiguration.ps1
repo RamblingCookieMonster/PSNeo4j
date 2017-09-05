7cf2f451f03e79a95edadb3e0b582345b6336b5c
RamblingCookieMonster <ramblingcookiemonster@users.noreply.github.com>
2017-09-05 13:01:09 -0400

    Add Set-PSNeo4jConfiguration per #5 and !Deploy

--- PSNeo4j/Public/Set-PSNeo4jConfiguration.ps1
+++ PSNeo4j/Public/Set-PSNeo4jConfiguration.ps1
@@ -1,4 +1,61 @@
 ﻿function Set-PSNeo4jConfiguration {
+    <#
+    .SYNOPSIS
+       Set PSNeo4j configuration values
+
+    .DESCRIPTION
+       Set PSNeo4j configuration values
+
+    .EXAMPLE
+        Set-PSNeo4jConfiguration -Credential $Credential
+
+        # Specify a default credential to use for PSNeo4j commands
+
+    .PARAMETER Scope
+        Which scope to serialize configuration to, when UpdateConfig is $True (default).
+
+        Defaults to 'User', allowing us to serialize credentials
+    
+    .PARAMETER UpdateConfig
+        Whether to update the configuration file on top of the live module values
+
+        Defaults to $True
+    
+    .PARAMETER As
+        Parse the Neo4j response as...
+            Parsed:  We attempt to parse the output into friendlier PowerShell objects
+                     Please open an issue if you see unexpected output with this
+            Raw:     We don't touch the response                           ($Response)
+            Results: We expand the 'results' property on the response      ($Response.results)
+            Row:     We expand the 'row' property on the responses results ($Response.results.data.row)
+
+        We default to 'Parsed' initially
+
+        See ConvertFrom-Neo4jResponse for implementation details
+    
+    .PARAMETER MetaProperties
+        Merge zero or any combination of these corresponding meta properties in the results: 'id', 'type', 'deleted'
+
+        We default to 'type' initially
+    
+    .PARAMETER MergePrefix
+        If any MetaProperties are specified, we add this prefix to avoid clobbering existing neo4j properties
+
+        We default to 'Neo4j' initially
+    
+    .PARAMETER BaseUri
+        BaseUri to build REST endpoint Uris from
+
+        We default to 'http://127.0.0.1:7474' initially
+    
+    .PARAMETER Credential
+        PSCredential to use for auth
+
+        No initial default
+    
+    .FUNCTIONALITY
+        Neo4j
+    #>
     [cmdletbinding()]
     param(
         [ValidateNotNull()]

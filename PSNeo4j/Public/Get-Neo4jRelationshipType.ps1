ae499cbfc8f0f6a95f37182401e8da88178124c8
RamblingCookieMonster <ramblingcookiemonster@users.noreply.github.com>
2017-09-06 22:18:25 -0400

    Add some procedure wrappers, still need #8

---
+++ PSNeo4j/Public/Get-Neo4jRelationshipType.ps1
@@ -0,0 +1,68 @@
+﻿function Get-Neo4jRelationshipType {
+    <#
+    .SYNOPSIS
+       Get relationship types in a Neo4j database
+
+    .DESCRIPTION
+       Get relationship types in a Neo4j database
+
+    .EXAMPLE
+        Get-Neo4jRelationshipType
+
+    .PARAMETER As
+        Parse the Neo4j response as...
+            Parsed:  We attempt to parse the output into friendlier PowerShell objects
+                     Please open an issue if you see unexpected output with this
+            Raw:     We don't touch the response                           ($Response)
+            Results: We expand the 'results' property on the response      ($Response.results)
+            Row:     We expand the 'row' property on the responses results ($Response.results.data.row)
+
+        We default to the value specified by Set-PSNeo4jConfiguration (Initially, 'Parsed')
+
+        See ConvertFrom-Neo4jResponse for implementation details
+
+    .PARAMETER MetaProperties
+        Merge zero or any combination of these corresponding meta properties in the results: 'id', 'type', 'deleted'
+
+        We default to the value specified by Set-PSNeo4jConfiguration (Initially, 'type')
+
+    .PARAMETER MergePrefix
+        If any MetaProperties are specified, we add this prefix to avoid clobbering existing neo4j properties
+
+        We default to the value specified by Set-PSNeo4jConfiguration (Initially, 'Neo4j')
+
+    .PARAMETER BaseUri
+        BaseUri to build REST endpoint Uris from
+
+        We default to the value specified by Set-PSNeo4jConfiguration (Initially, 'http://127.0.0.1:7474')
+
+    .PARAMETER Credential
+        PSCredential to use for auth
+
+        We default to the value specified by Set-PSNeo4jConfiguration (Initially, not specified)
+
+    .FUNCTIONALITY
+        Neo4j
+    #>
+    [cmdletbinding()]
+    param(
+        [validateset('Raw', 'Results', 'Row', 'Parsed')]
+        [string]$As = $PSNeo4jConfig.As,
+        [validateset('id', 'type', 'deleted')]
+        [string]$MetaProperties = $PSNeo4jConfig.MetaProperties,
+        [string]$MergePrefix = $PSNeo4jConfig.MergePrefix,
+
+        [string]$BaseUri = $PSNeo4jConfig.BaseUri,
+        [ValidateNotNull()]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]
+        $Credential =  $PSNeo4jConfig.Credential  
+    )
+    $InvokeParams = @{}
+    $Query = "CALL db.relationshipTypes()"
+    $InvokeParams.add('Query', $Query)
+    Write-Verbose "Query: [$Query]"
+
+    $Params = . Get-ParameterValues -Properties MetaProperties, MergePrefix, Credential, BaseUri, As
+    Invoke-Neo4jQuery @Params @InvokeParams
+}
